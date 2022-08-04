1bbb9f6bbf1a178e7c0e7ccfa2c2c10c77ab8bfb
jbenavidesg27 <jbenavidesg27@gmail.com>
2022-08-04 17:44:11 -0500

    CheckStyle terminado

--- src/main/java/com/nttdata/bootcamp/ConfigApplication.java
+++ src/main/java/com/nttdata/bootcamp/ConfigApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Application config and eureka server.
+ *
+ */
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigApplication.class, args);
+  }
 
 }
